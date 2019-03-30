a5f14c0263058d78f445e2758748f9d8d2861776
Rebecca Martin <rebecca-martin@live.com>
2019-03-30 11:12:57 -0700

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import s_query, { add_to_dom, make_element } from './helpers';
 import add from './add';
 
-add_to_dom('hello there!');
+add_to_dom('hello there! webpack is fun!');
 
 add_to_dom(`5 + 8 = ${add(5, 8)}`);
 
 make_element('p', 'this is a paragraph tag');
 
 s_query.make_element('h2', 'this is s_query');
+
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname),
+        publicPath: '/dist'
     }
 
 };
